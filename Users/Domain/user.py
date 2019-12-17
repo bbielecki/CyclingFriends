0c4f93bb0b67101d90595e3c7a7ac07292f43705
bartekactivy <bartek@activy.pl>
2019-12-17 23:28:13 +0100

    Update user model

--- Users/Domain/user.py
+++ Users/Domain/user.py
@@ -1,6 +1,11 @@
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
-class User():
+Base = declarative_base()
+
+
+class User(Base):
     __tablename__ = 'players.AUsers'
 
     Id = Column(String, primary_key=True)
@@ -9,6 +14,8 @@ class User():
     City = Column(String)
     DateOfBirth = Column(DateTime)
     UserType = Column(Integer)
+    UserBadges = relationship("AUserBadges", schema="players", back_populates="AUsers")
+    PersonalStatistics = relationship("APersonalStatistics", schema="players", back_populates="AUsers")
 
     def __repr__(self):
         return "<User(Id='%s', Wight='%s', DateOfBirth='%s', City='%s'>" \
