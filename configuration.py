8c5933b9ea3fca52d48fec9591cb130f2682790f
marcin-jamroz <m.jamroz18@gmail.com>
2020-01-05 18:53:11 +0100

    linux configuration 1

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 
-db_config = {'connection_string': 'sa:Passw12#@127.0.0.1,11433/activy_anonimized?driver=SQL+Server+Native+Client+11.0'}
+db_config = {'connection_string': 'sa:Passw12#@127.0.0.1,11433/activy_anonimized?driver=ODBC+Driver+17+for+SQL+Server'}
