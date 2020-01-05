804bbc0466bd8d793d2ad0b5ebb94dc0383a5080
Marcin Jamroz <marcin.jamroz@allbright.io>
2020-01-05 20:34:07 +0100

    linux configuration 2

--- configuration.py
+++ configuration.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
 
-db_config = {'connection_string': 'sa:Passw12#@127.0.0.1,11433/activy_anonimized?driver=ODBC+Driver+17+for+SQL+Server'}
+# windows
+# db_config = {'connection_string': 'sa:Passw12#@127.0.0.1,11433/activy_anonimized?driver=SQL+Server+Native+Client+11.0'}
+
+#linux
+db_config = {'connection_string': 'sa:Passw12#@127.0.0.1/activy_anonimized?driver=ODBC+Driver+17+for+SQL+Server'}
