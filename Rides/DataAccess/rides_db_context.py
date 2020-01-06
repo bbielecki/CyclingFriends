51e5fc36938a920b7bbaf6b9af9ebeaf8f4883f4
Marcin Jamroz <marcin.jamroz@allbright.io>
2020-01-06 19:46:42 +0100

    Rides table

--- Rides/DataAccess/rides_db_context.py
+++ Rides/DataAccess/rides_db_context.py
@@ -1,10 +1,19 @@
 import Services.db_connection as connection
+from sqlalchemy.orm import sessionmaker
 
 db = connection.db
 engine = connection.engine
 metadata = db.MetaData(bind=engine)
-players = db.Table('APlayers', metadata, schema="rides",  autoload=True, autoload_with=engine)
-player_rides = db.Table('APlayerRide', metadata, schema="rides",  autoload=True, autoload_with=engine)
-rides = db.Table('ARides', metadata, schema="rides",  autoload=True, autoload_with=engine)
-strava_athletes = db.Table('AStravaAthletes', metadata, schema="rides",  autoload=True, autoload_with=engine)
-garmin_athletes = db.Table('AGarminAthletes', metadata, schema="rides",  autoload=True, autoload_with=engine)
+Session = sessionmaker()
+Session.configure(bind=engine)
+
+players = db.Table('APlayers', metadata, schema="rides",
+                   autoload=True, autoload_with=engine)
+player_rides = db.Table('APlayerRide', metadata,
+                        schema="rides",  autoload=True, autoload_with=engine)
+rides = db.Table('ARides', metadata, schema="rides",
+                 autoload=True, autoload_with=engine)
+strava_athletes = db.Table('AStravaAthletes', metadata,
+                           schema="rides",  autoload=True, autoload_with=engine)
+garmin_athletes = db.Table('AGarminAthletes', metadata,
+                           schema="rides",  autoload=True, autoload_with=engine)
--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
 import Users.users_qh as users
 import Users.personal_stats_qh as personal_stats
+import Rides.rides_qh as rides
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    print("users:")
-    print(users.get_users())
-    print("stats:")
-    print(personal_stats.get_stats())
+    # print("users:")
+    # print(users.get_users())
+    # print("stats:")
+    # print(personal_stats.get_stats())
+    print(rides.get_rides())
     return 'Hello World!'
 
 
