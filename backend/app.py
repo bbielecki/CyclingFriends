79f8262ec7145ca1a5145991961b9716af6425ff
Marcin Jamroz <marcin.jamroz@allbright.io>
2020-01-15 00:37:21 +0100

    add_pois method - insert into database pois for all clusters centers

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-import Users.users_qh as users
-import Users.personal_stats_qh as personal_stats
-import Rides.rides_qh as rides
-from osm.get_poi import get_poi
-import Graphs.rides_graph as rides_graph
+# import Users.users_qh as users
+# import Users.personal_stats_qh as personal_stats
+# import Rides.rides_qh as rides
+# import Graphs.rides_graph as rides_graph
+
+import Rides.pois_qh as pois
 
 app = Flask(__name__)
 CORS(app)
@@ -13,12 +14,7 @@ CORS(app)
 
 @app.route('/')
 def hello_world():
-    # print("users:")
-    # print(users.get_users())
-    # print("stats:")
-    # print(personal_stats.get_stats())
-    # print(rides.get_rides())
-    print(get_poi(54.349416, 18.648098))
+    pois.add_pois()
     return 'Hello World!'
 
 
--- backend/configuration.py
+++ backend/configuration.py
@@ -1,9 +1,5 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
-import os
-# windows
-print(os.getenv("CONNECTION_STRING"))
-db_config = {'connection_string': os.getenv("CONNECTION_STRING")}
 
-# linux
-# db_config = {'connection_string': 'sa:Passw12#@127.0.0.1/activy_anonimized?driver=ODBC+Driver+17+for+SQL+Server'}
+db_config = {'connection_string': os.getenv("CONNECTION_STRING")}
